5edcaf2a6eba6ece4a24cac12fa7d189ae23a51f
dievit <125148142+dievit@users.noreply.github.com>
2023-05-03 08:47:20 -0300

    Alterações sprint 3
    Alterações de cores, CSS e HTML

--- app.py
+++ app.py
@@ -30,25 +30,5 @@ def spbacklog():
 def burndown():
     return render_template('burndown.html')
 
-@app.route('/artefatos_arquivos_para_download')
-def artefatos_download():
-    return render_template('artefatosdownload.html')
-
-@app.route('/avaliacao')
-def avaliacao():
-    return render_template('avaliacao.html')
-
-@app.route('/eventos_scrum')
-def eventos_scrum():
-    return render_template('eventos-scrum.html')
-
-@app.route('/scrumcases')
-def scrum_cases():
-    return render_template('scrumcases.html')
-
-@app.route('/softskills')
-def softskills():
-    return render_template('softskills.html')
-
 if __name__=='__main__':
     app.run(debug=True, use_debugger=True, use_reloader=True)
